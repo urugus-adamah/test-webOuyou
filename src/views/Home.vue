4ca15ad6114c5b3b5914f4f7f4ddee29a7708970
hamada <zc73s.masuwari@gmail.com>
2021-03-06 09:34:06 +0900

    post methods modified

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,8 @@
   <div class="home">
     <input type="text" v-model="word">
     <input type="submit" @click="send">
+    <p>data1は{{data1}}</p>
+    <p>data2は {{data2}}</p>
     <p>{{text}}</p>
   </div>
 </template>
@@ -13,16 +15,21 @@ export default {
     return {
       word:"",
       text:"",
+      data1:"",
+      data2:""
     };
   },
   methods :{  
     send(){
       console.log("enter_send");
       axios
-        .post("https://gentle-peak-42861.herokuapp.com/api",data:this.word)
+        .post("https://gentle-peak-42861.herokuapp.com/api",{data:this.word})
         .then((response)=>{
-          console.log(response.data);
-          this.text=response.data.data;
+          // console.log(response.data);
+          // console.log(response.data.message);
+          this.data1 = response.data;
+          this.data2 = response.data.data;
+          this.text=response.data.data.data;
         });
         console.log("out_send");
     }
